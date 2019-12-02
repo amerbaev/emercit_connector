af487d2c2fdb00a8440d131aebdf91b5161ffb38
Arseny Amerbaev <amerbaev@gmail.com>
2019-12-02 19:16:49 +0300

    progressbars

--- provider.py
+++ provider.py
@@ -4,6 +4,8 @@ from connector import EmercitConnector
 from mongo import EmercitMongo
 from mappings import map_kwargs
 
+from tqdm import tqdm
+
 
 class EmercitProvider:
     def __init__(self, connector: EmercitConnector = None, mongo: EmercitMongo = None):
@@ -16,7 +18,9 @@ class EmercitProvider:
         self._mongo = mongo
 
     def dump_all(self, from_date: date, to_date: date = date.today(), fields: list = None):
+        print('Getting all features')
         features = self._connector.overall()
+        print('Saving features')
         self._mongo.save_features(features)
 
         exists_modes = {}
@@ -32,10 +36,10 @@ class EmercitProvider:
                     exists_modes[feature_id].append(map_kwargs(field))
 
         days_delta = (to_date - from_date).days
-        for delta in range(0, days_delta + 1, 50):
+        for delta in tqdm(range(0, days_delta + 1, 50)):
             date_from = from_date + timedelta(days=delta)
             date_to = from_date + timedelta(days=delta + 49)
-            for feature_id, modes in exists_modes.items():
+            for feature_id, modes in tqdm(exists_modes.items()):
                 for kwargs in modes:
                     try:
                         measurements, _, _ = self._connector.mgraph(station_id=feature_id,
@@ -51,7 +55,7 @@ class EmercitProvider:
                                                       mode=kwargs['mode'],
                                                       measurements=measurements)
 
-
 if __name__ == '__main__':
-    EmercitProvider().dump_all(from_date=date(year=2019, month=1, day=1),
-                               fields=['river_level', 'precipitation', 'temperature', 'humidity'])
+    mongo = EmercitMongo(host='10.101.101.2')
+    EmercitProvider(mongo=mongo).dump_all(from_date=date(year=2019, month=1, day=1),
+                                          fields=['river_level', 'temperature', 'humidity'])
