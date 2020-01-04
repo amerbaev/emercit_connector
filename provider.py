d7d7bbe729ab49cfef5b8b465a3242e743afa8c5
Arseny Amerbaev <amerbaev@group-ib.com>
2020-01-04 21:35:58 +0300

    multi-threading

--- provider.py
+++ provider.py
@@ -1,10 +1,16 @@
+import logging
+import concurrent.futures
 from datetime import date, timedelta
+from typing import List, Tuple, Dict, Any
 
 from connector import EmercitConnector
 from mongo import EmercitMongo
 from mappings import map_kwargs
 
 from tqdm import tqdm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 
 class EmercitProvider:
@@ -18,14 +24,14 @@ class EmercitProvider:
         self._mongo = mongo
 
     def dump_all(self, from_date: date, to_date: date = date.today(), fields: list = None):
-        print('Getting all features')
+        logger.info('Getting all features')
         features = self._connector.overall()
-        print('Saving features')
+        logger.info('Saving features')
         self._mongo.save_features(features)
 
         exists_modes = {}
-        for feature in features:
-            properties = feature['properties']
+        for future in features:
+            properties = future['properties']
             feature_id = properties['id']
             for field, val in properties['data'].items():
                 if fields is not None and field not in fields:
@@ -36,26 +42,50 @@ class EmercitProvider:
                     exists_modes[feature_id].append(map_kwargs(field))
 
         days_delta = (to_date - from_date).days
-        for delta in tqdm(range(0, days_delta + 1, 50)):
-            date_from = from_date + timedelta(days=delta)
-            date_to = from_date + timedelta(days=delta + 49)
-            for feature_id, modes in tqdm(exists_modes.items()):
-                for kwargs in modes:
+        intervals: List[Tuple[date, date]] = []
+        for delta in range(0, days_delta + 1, 1):
+            interval_start = from_date + timedelta(days=delta)
+            interval_end = from_date + timedelta(days=delta)
+            if interval_end > to_date:
+                interval_end = to_date
+            intervals.append((interval_start, interval_end))
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            interval_measurements = {executor.submit(self.get_measurements, start, end, exists_modes): (start, end)
+                                     for start, end in intervals}
+            with tqdm(total=len(interval_measurements)) as pbar:
+                for future in concurrent.futures.as_completed(interval_measurements):
+                    logger.debug("Next result is coming")
+                    interval = interval_measurements[future]
                     try:
-                        measurements, _, _ = self._connector.mgraph(station_id=feature_id,
-                                                                    date_from=date_from,
-                                                                    date_to=date_to,
-                                                                    **kwargs)
-                    except Exception as e:
-                        print("Can't get measurements for station {} from {} to {} with params {}"
-                              .format(feature_id, date_from, date_to, kwargs))
-                        print(e)
-                    else:
-                        self._mongo.save_measurements(station_id=feature_id,
-                                                      mode=kwargs['mode'],
-                                                      measurements=measurements)
+                        future.result()
+                    except:
+                        logger.debug("Can't get measurements for interval: ", str(interval[0]), "-", str(interval[1]))
+                    finally:
+                        pbar.update(1)
+
+    def get_measurements(self, date_from, date_to, exists_modes) -> Dict[str, Dict[str, Any]]:
+        measurements = {}
+        for feature_id, modes in exists_modes.items():
+            for kwargs in modes:
+                try:
+                    measure, _, _ = self._connector.mgraph(station_id=feature_id,
+                                                           date_from=date_from,
+                                                           date_to=date_to,
+                                                           **kwargs)
+                    if feature_id not in measurements:
+                        measurements[feature_id] = {}
+                    self._mongo.save_measurements(station_id=feature_id,
+                                                  mode=kwargs['mode'],
+                                                  measurements=measure)
+                except Exception as e:
+                    logger.warning("Can't get measurements for station {} from {} to {} with params {}"
+                                   .format(feature_id, date_from, date_to, kwargs))
+                    logger.error(e)
+        return measurements
+
 
 if __name__ == '__main__':
-    mongo = EmercitMongo(host='10.101.101.2')
-    EmercitProvider(mongo=mongo).dump_all(from_date=date(year=2019, month=1, day=1),
+    mongo = EmercitMongo(host='192.168.5.203')
+    EmercitProvider(mongo=mongo).dump_all(from_date=date(year=2014, month=1, day=1),
                                           fields=['river_level', 'temperature', 'humidity'])
